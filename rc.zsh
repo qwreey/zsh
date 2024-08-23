0dd43cd0a73652122423a392e50260ae5954b8af
Qwreey <qwreey75@gmail.com>
2024-08-23 18:48:08 +0900

    Update rc.zsh

--- rc.zsh
+++ rc.zsh
@@ -32,7 +32,7 @@ source "$ZSHDIR/defer/zsh-defer.plugin.zsh"
 
 # pyenv
 if (( $+commands[cygpath] )); then
-	export PATH="$ZSHDIR/pyenv-win/bin:$ZSHDIR/pyenv/pyenv-win/shims:$PATH"
+	export PATH="$ZSHDIR/pyenv/pyenv-win/bin:$ZSHDIR/pyenv/pyenv-win/shims:$PATH"
 else
 	[ -e "$ZSHDIR/pyenv" ] && zsh-defer +1 +2 -c 'eval "$($ZSHDIR/pyenv/bin/pyenv init -)"; eval "$($ZSHDIR/pyenv/bin/pyenv virtualenv-init -)"'
 fi
