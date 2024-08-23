ee2e4535392ff86315e179e0d9161d42ec6c9b8e
Qwreey <qwreey75@gmail.com>
2024-08-23 16:38:15 +0900

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -40,8 +40,8 @@ log "Clone zsh-users/zsh-autosuggestions"     ; git clone https://github.com/zsh
 if [ "$ZSHPYENV" = "true" ]; then
     log "Install pyenv"
     curl --proto '=https' --tlsv1.2 -sSf https://pyenv.run | PYENV_ROOT="$ZSHDIR/pyenv" bash
-    # eval "$($ZSHDIR/pyenv/bin/pyenv init -)"
-    # eval "$($ZSHDIR/pyenv/bin/pyenv virtualenv-init -)"
+    eval "$($ZSHDIR/pyenv/bin/pyenv init -)"
+    eval "$($ZSHDIR/pyenv/bin/pyenv virtualenv-init -)"
     PYENV_ROOT="$ZSHDIR/pyenv" $ZSHDIR/pyenv/bin/pyenv install 3.12
     PYENV_ROOT="$ZSHDIR/pyenv" $ZSHDIR/pyenv/bin/pyenv virtualenv 3.12 default
     PYENV_ROOT="$ZSHDIR/pyenv" $ZSHDIR/pyenv/bin/pyenv activate default
